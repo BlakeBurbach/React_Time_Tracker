85e2a0d78f4e0f05d8ed54d6e9c6049f4a164996
Blake Burbach <cras.bburbach@gmail.com>
2018-06-21 11:43:20 -0500

    ProjectSelector selecting projects

--- src/components/EntryPage/EntryPageComponents/ProjectSelector.js
+++ src/components/EntryPage/EntryPageComponents/ProjectSelector.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 class ProjectSelector extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            project_name: ''
+        }
+    }
+
+    handleProjectSelect = (event) => {
+        this.setState({
+            project_name: event.target.value
+        });
+    }
+
     render() {
         return (
             <div className="entryPageInput">
                 <label>Project </label>
-                <select>
-                    <option>project</option>
+                <select value={this.state.project_name} onChange={this.handleProjectSelect}>
+                    <option>Select One</option>
+                    <option value="project_1">project 1</option>
+                    <option value="project_2">project 2</option>
+                    <option value="project_3">project 3</option>
+                    <option value="project_4">project 4</option>
                 </select>
             </div>
         )
