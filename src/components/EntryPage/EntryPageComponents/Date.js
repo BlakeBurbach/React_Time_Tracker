c555e381ade43b36cf5a6f08eab0bab6ca074aa4
Blake Burbach <cras.bburbach@gmail.com>
2018-06-21 12:17:13 -0500

    date selector selecting dates

--- src/components/EntryPage/EntryPageComponents/Date.js
+++ src/components/EntryPage/EntryPageComponents/Date.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Date extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            date: ''
+        }
+    }
+
+    handleDate = (event) => {
+        this.setState({
+            date: moment(event.target.value).format('YYYY-MM-DD')
+        }, ()=>{
+            console.log('Date select', this.state.date);
+        })
+    }
+
     render() {
         return (
             <div className="entryPageInput">
                 <label>Date</label>
-                <input type="date" />
+                <input type="date" value={this.state.date} onChange={this.handleDate}/>
             </div>
         )
     }
