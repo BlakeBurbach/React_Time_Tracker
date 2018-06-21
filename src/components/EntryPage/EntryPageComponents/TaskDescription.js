4fb20d6863dde535dd6f54fbe540f700942268d7
Blake Burbach <cras.bburbach@gmail.com>
2018-06-21 11:37:56 -0500

    TaskDescription setting it's state

--- src/components/EntryPage/EntryPageComponents/TaskDescription.js
+++ src/components/EntryPage/EntryPageComponents/TaskDescription.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class TaskDescription extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            task_description: ''
+        }
+    }
+
+    handleTaskInput = (event) => {
+        this.setState({
+            task_description: event.target.value
+        });
+    }
+
     render() {
         return (
             <div className="entryPageInput">
             <label>Task </label>
-                <input type="text" placeholder="description" />
+                <input type="text" placeholder="description" value={this.state.task_description} onChange={this.handleTaskInput}/>
             </div>
         )
     }
