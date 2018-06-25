66d1f6813a3f8018381102e2d7f222a5a9122ebf
Blake Burbach <cras.bburbach@gmail.com>
2018-06-25 15:42:06 -0500

    started a GET request for projects:

---
+++ server/routes/Projects.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// GET route to retrieve all projects from database
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM projects WHERE project_user_id = $1;`;
+    pool.query(queryText, [userID]).then((result) => {
+        console.log('ProjectRouter GET success', result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('ERROR in ProjectRouter GET route', error);
+        res.sendStatus(500);
+    }); // end pool.query
+}); // end GET route
+
+// POST route to add new Projects to database
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const TaskEntryRouter = require('./routes/TaskEntries.router.js');
+const ProjectRouter = require('./routes/Projects.router.js');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +26,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/task_entries', TaskEntryRouter);
+app.use('/api/projects', ProjectRouter);
 
 // Serve static files
 app.use(express.static('build'));
